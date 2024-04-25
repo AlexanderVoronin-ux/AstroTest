567039cca272c3194ab857a09c4399e9c5c3fd3a
alexander <alexvoronin021083@gmail.com>
2024-04-25 16:39:55 +0300

    Feature: added reactotron

--- app/navigation/MainNavigator.tsx
+++ app/navigation/MainNavigator.tsx
@@ -14,7 +14,6 @@ const MainStack = createNativeStackNavigator<RootParamList>()
 
 export const MainNavigator = () => {
   const {data} = useGetPokemonByNameQuery()
-
   return (
     <NavigationContainer>
       <MainStack.Navigator
